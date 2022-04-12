5b846bfa38541147597b62e1fe493972ef920eee
Jean Mário Moreira de Lima <jeanmariomlima@gmail.com>
2022-04-12 15:50:49 -0300

    mod main

--- f5_traveler/lib/screens/countries_places_screen.dart
+++ f5_traveler/lib/screens/countries_places_screen.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class CountryPlacesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lugares para Conhecer'),
+        //teste
+        //teste
+        //teste
+      ),
+    );
   }
 }
