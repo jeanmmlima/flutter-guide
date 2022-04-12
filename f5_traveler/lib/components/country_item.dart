07feffcbc079d3cea3f5104bc01f8f66428f0432
Jean Mário Moreira de Lima <jeanmariomlima@gmail.com>
2022-04-12 16:18:05 -0300

    navegando para nova tela e passando dados via construtor

--- f5_traveler/lib/components/country_item.dart
+++ f5_traveler/lib/components/country_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import '../screens/countries_places_screen.dart';
 
 import '../models/country.dart';
 
@@ -7,24 +8,43 @@ class CountryItem extends StatelessWidget {
   final Country country;
 
   const CountryItem(this.country);
+
+  void _selectCountry(BuildContext context) {
+    //usar o navigator para fazer o push
+    //colocar mais uma tela na nossa pilha de telas
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CountryPlacesScreen(country);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Text(
-        country.title,
-        style: Theme.of(context).textTheme.headline6,
+    return InkWell(
+      //tornando elemento clicável
+      onTap: () => _selectCountry(context),
+      splashColor: Theme.of(context).primaryColor, //cor do clique
+      borderRadius: BorderRadius.circular(15), //clique dentro da área esperada
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          country.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+                colors: [
+                  country.color.withOpacity(0.5), //cor passad com opacidade
+                  country.color, //cor passada
+                ],
+                begin: Alignment.topLeft, //inicio do gradiente
+                end: Alignment.bottomRight // fim
+                )),
       ),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          gradient: LinearGradient(
-              colors: [
-                country.color.withOpacity(0.5), //cor passad com opacidade
-                country.color, //cor passada
-              ],
-              begin: Alignment.topLeft, //inicio do gradiente
-              end: Alignment.bottomRight // fim
-              )),
     );
   }
 }
--- f5_traveler/lib/screens/countries_places_screen.dart
+++ f5_traveler/lib/screens/countries_places_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../models/country.dart';
+
 class CountryPlacesScreen extends StatelessWidget {
+  final Country country;
+
+  const CountryPlacesScreen(this.country);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lugares para Conhecer'),
+        title: Text(country.title),
       ),
-      body: Container(
-        child: Text('Teste'),
+      body: Center(
+        child: Text('Lugares por País ${country.id}'),
       ),
     );
   }
