33fe75664f1f560c6bd9c6e9f13fcad9e0a7a241
jeanmmlima <jeanmariomlima@gmail.com>
2022-05-10 15:43:14 -0300

    sm counter ok

--- f6_sm_counter/lib/main.dart
+++ f6_sm_counter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:f6_sm_counter/models/CounterModel.dart';
+import 'package:f6_sm_counter/screens/CounterSM.dart';
 import 'package:f6_sm_counter/screens/SecondScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,56 +16,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CounterSM(),
-      
-    );
-  }
-}
-
-class CounterSM extends StatelessWidget {
-  _showSecondScreen(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-      return SecondScreen();
-    }));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Counter MS'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Meu Contador:'),
-            Consumer<CounterModel>(
-              builder: (context, counter, child) {
-                return Text(
-                  "Contador: ${counter.countValue}",
-                  style: TextStyle(fontSize: 25),
-                );
-              },
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            ElevatedButton(
-              onPressed: _showSecondScreen(context),
-              child: Text('Segunda tela'),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          final contador = context.read<CounterModel>();
-          contador.add();
-        },
-      ),
-    );
+    return MaterialApp(initialRoute: '/', routes: {
+      '/': (ctx) => CounterSM(),
+      '/second-screen': (ctx) => SecondScreen(),
+    });
   }
 }
