95126a21bbf87bcc22f39649805b3292ecd590b3
Jean Mário Moreira de Lima <jeanmariomlima@gmail.com>
2022-05-12 14:37:53 -0300

    novo porjeto

---
+++ f7_provider_shopper/lib/main.dart
@@ -0,0 +1,73 @@
+import 'dart:io';
+
+import 'package:f7_provider_shopper/models/carrinho.dart';
+import 'package:f7_provider_shopper/models/catalogo.dart';
+import 'package:f7_provider_shopper/utils/theme.dart';
+import 'package:f7_provider_shopper/views/carrinho.dart';
+import 'package:f7_provider_shopper/views/catalogo.dart';
+import 'package:f7_provider_shopper/views/login.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:window_size/window_size.dart';
+
+void main() {
+  setupWindow();
+  runApp(const MyApp());
+}
+
+const double windowWidth = 400;
+const double windowHeight = 800;
+
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Provider Demo');
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    getCurrentScreen().then((screen) {
+      setWindowFrame(Rect.fromCenter(
+        center: screen!.frame.center,
+        width: windowWidth,
+        height: windowHeight,
+      ));
+    });
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Using MultiProvider is convenient when providing multiple objects.
+    return MultiProvider(
+      providers: [
+        // In this sample app, CatalogModel never changes, so a simple Provider
+        // is sufficient.
+        Provider(create: (context) => CatalogModel()),
+        // CartModel is implemented as a ChangeNotifier, which calls for the use
+        // of ChangeNotifierProvider. Moreover, CartModel depends
+        // on CatalogModel, so a ProxyProvider is needed.
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            if (cart == null) throw ArgumentError.notNull('cart');
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Provider Demo',
+        theme: appTheme,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const MyLogin(),
+          '/catalog': (context) => const MyCatalog(),
+          '/cart': (context) => const MyCart(),
+        },
+      ),
+    );
+  }
+}
---
+++ f7_provider_shopper/lib/models/carrinho.dart
@@ -0,0 +1,33 @@
+import 'package:f7_provider_shopper/models/catalogo.dart';
+import 'package:flutter/foundation.dart';
+
+class CartModel extends ChangeNotifier {
+  late CatalogModel _catalog;
+
+  final List<int> _itemIds = [];
+
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    _catalog = newCatalog;
+
+    notifyListeners();
+  }
+
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  int get totalPrice =>
+      items.fold(0, (total, itemCorrente) => total + itemCorrente.price);
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+
+    notifyListeners();
+  }
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+
+    notifyListeners();
+  }
+}
