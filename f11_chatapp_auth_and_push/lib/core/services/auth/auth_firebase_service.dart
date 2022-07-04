38140e82b306b242fadff03aa453ca2ad09dadc7
jeanmmlima <jeanmariomlima@gmail.com>
2022-07-04 11:17:39 -0300

    push notifications

--- f11_chatapp_auth_and_push/lib/core/services/auth/auth_firebase_service.dart
+++ f11_chatapp_auth_and_push/lib/core/services/auth/auth_firebase_service.dart
@@ -58,7 +58,8 @@ class AuthFirebaseService implements AuthService {
     await credential.user?.updatePhotoURL(imageURL);
 
     //3 salvar banco NoSQL Firestore
-    await _saveChatUser(_toChatUser(credential.user!, imageURL));
+    _currentUser = _toChatUser(credential.user!, name, imageURL);
+    await _saveChatUser(_currentUser!);
   }
 
   @override
@@ -95,10 +96,10 @@ class AuthFirebaseService implements AuthService {
     });
   }
 
-  static ChatUser _toChatUser(User user, [String? imageURL]) {
+  static ChatUser _toChatUser(User user, [String? name, String? imageURL]) {
     return ChatUser(
       id: user.uid,
-      name: user.displayName ?? user.email!.split('@')[0],
+      name: name ?? user.displayName ?? user.email!.split('@')[0],
       email: user.email!,
       imageURL: imageURL ?? user.photoURL ?? 'assets/images/avatar.png',
     );
